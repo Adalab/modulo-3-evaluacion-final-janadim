89ce3a62e0b1e0196be960894f7cce32535fcf22
janadim <jana.d.titarchuk@gmail.com>
2021-06-16 14:06:09 +0200

    add handler and filter for name input

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import getApiData from "../services/api";
-import Filters from "./FilterByName";
+import FilterByName from "./FilterByName";
 import CharacterList from "./CharacterList";
 import ls from "../services/local-storage";
 
 function App() {
   const charsLocalStorageData = ls.get("chars", []);
   const [chars, setChars] = useState(charsLocalStorageData);
-  // const [filterName, setfilterName] = useState(ls.get("filterName", ""));
+  const [filterName, setFilterName] = useState(ls.get("filterName", ""));
+  console.log(filterName);
+  //useStates
 
   useEffect(() => {
     if (chars.length === 0) {
@@ -21,11 +23,23 @@ function App() {
     ls.set("chars", chars);
   }, [chars]);
 
+  //handlers
+
+  const handleFilter = (ev) => {
+    setFilterName(ev.target.value);
+  };
+
+  //render
+  const filteredChars = chars.filter((char) => {
+    console.log(filterName);
+    return char.name.toLowerCase().includes(filterName);
+  });
+
   return (
     <>
       <h1>Rick and Morty</h1>
-      <Filters />
-      <CharacterList chars={chars} />
+      <FilterByName handleFilter={handleFilter} />
+      <CharacterList chars={filteredChars} />
     </>
   );
 }
--- src/components/FilterByName.js
+++ src/components/FilterByName.js
@@ -1,14 +1,8 @@
 const FilterByName = (props) => {
-  const handleChange = (ev) => {
-    props.handleFilter({
-      value: ev.target.value,
-      key: "name",
-    });
-  };
   return (
     <>
       <label htmlFor="name">Search:</label>
-      <input type="text" name="name" id="name" onChange={handleChange} />
+      <input type="text" name="name" id="name" onChange={props.handleFilter} />
     </>
   );
 };
