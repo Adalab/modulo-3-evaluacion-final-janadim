434dda2dc60e935b0959c61326f5c61daa408c30
janadim <jana.d.titarchuk@gmail.com>
2021-06-24 21:07:02 +0200

    change some css styles for cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 const CharacterCard = (props) => {
   return (
     <Link to={`/character/${props.eachChar.id}`}>
-      <article className="li-item">
+      
+        
+      <div className="card">
         <img
           src={props.eachChar.image}
           alt={props.eachChar.name}
           title={props.eachChar.name}
           className="li-img"
         />
-
         <h4 className="charName">{props.eachChar.name}</h4>
         <p className="charSpec">{props.eachChar.species}</p>
-      </article>
+      </div>
+        
+      
     </Link>
   );
 };
